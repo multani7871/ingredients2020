1cc76c2af4b5b5a6998b7a82de25f86d3eaf1857
Harvey Multani <harvey.multani@gmail.com>
2017-08-10 12:54:52 -0700

    require body parser and enable for json parsing

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var db = require('./db/config.js');
 var User = require('./db/models/users.js');
 
 var app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'build')));
 
 // app.get('/', function(req, res) {
