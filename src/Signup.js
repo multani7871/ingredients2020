1fccdf10733ec18c8d7b8d37c7a6041c9213ec5e
fredesse <eleegia@gmail.com>
2017-08-10 15:02:59 -0700

    Implemented POST method on signup page

--- src/Signup.js
+++ src/Signup.js
@@ -3,22 +3,34 @@ import { Link } from 'react-router-dom';
 import './App.css';
 
 class Signup extends Component {
+
+  submitUser(e) {
+    e.preventDefault();
+
+    var data = {
+      username: this.state.email,
+      password: this.state.password
+    }
+
+    $.post('http://localhost:3000/api/user', {
+      data: data
+    })
+    .fail(function() {
+      console.log('Signup failed');
+    });
+  }
+
   render() {
     return (
       <div>
         <div>
           <h2>Welcome to Ingredients 20/20</h2>
         </div>
-        <div>
-          <input type="email" />
-          <input type="password" />
-        </div>
-        <div>
-          <ul>
-            <li><Link to="/login">LOGIN</Link></li>
-            <li><Link to="/signup">SIGNUP</Link></li>
-          </ul>
-        </div>
+        <form onSubmit={this.submitUser} >
+          <input type="email" value={this.state.email} />
+          <input type="password" value={this.state.password} />
+          <input type="submit" value="Submit"/>
+        </form>
       </div>
     );
   }
