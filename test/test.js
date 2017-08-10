fc986d01800065a13315a138848e85535a54f66e
Harvey Multani <harvey.multani@gmail.com>
2017-08-09 17:55:24 -0700

    Add two server tests

---
+++ test/test.js
@@ -0,0 +1,27 @@
+var request = require('supertest');
+var express = require('express');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+var expect = require('chai').expect;
+var app = require('../server.js');
+
+chai.use(chaiHttp);
+
+it('should serve the home page', (done) => {
+  chai.request(app)
+  .get('/')
+  .end((err, res) => {
+    res.should.have.status(200);
+    done();
+  });
+});
+
+it('should not serve the login page since we it doesn\'t exist', (done) => {
+  chai.request(app)
+  .get('/login')
+  .end((err, res) => {
+    res.should.have.status(404);
+    done();
+  });
+});
