27f613690a49fffa8ada93fbb8cb404565bb40b5
fredesse <eleegia@gmail.com>
2017-08-09 19:41:16 -0700

    Creat static files and React component stubs

---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Welcome to Ingredients 20/20</h2>
+        </div>
+        <button>LOGIN</button>
+        <button>SIGNUP</button>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/Signup.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Signup extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>Welcome to Ingredients 20/20</h2>
+        </div>
+        <button>LOGIN</button>
+        <button>SIGNUP</button>
+      </div>
+    );
+  }
+}
+
+export default Signup;
