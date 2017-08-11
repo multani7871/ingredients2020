40129f64f0e08ef222f9387dbc78052dfa4bff4f
fredesse <eleegia@gmail.com>
2017-08-10 19:35:53 -0700

    Created login post method

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,53 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import $ from 'jquery';
 import './App.css';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.handleUsername = this.handleUsername.bind(this);
+    this.handlePassword = this.handlePassword.bind(this);
+    this.LogUserIn = this.LogUserIn.bind(this);
+  }
+
+  handleUsername(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  handlePassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  LogUserIn(e) {
+    console.log(this.state);
+    e.preventDefault();
+
+    var data = {
+      username: this.state.username,
+      password: this.state.password
+    }
+
+    $.post('http://localhost:8000/api/users', {
+      data: data
+    })
+    .done(() => {
+      console.log('user was logged in')
+    })
+    .fail(() => {
+      console.log('Login failed')
+    })
+  }
+
   render() {
     return (
       <div>
