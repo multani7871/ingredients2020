1c768a6958b3f2d13638f69283485dd9a3654512
Harvey Multani <harvey.multani@gmail.com>
2017-08-15 01:39:29 -0700

    Move route logic from server-config to route-handler

--- server-config.js
+++ server-config.js
@@ -1,86 +1,22 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var db = require('./db/config.js');
-var User = require('./db/models/users.js');
-var Ingredient = require('./db/models/ingredients.js');
+
+var handler = require('./route-handler');
 
 var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'build')));
 
 //user creation api route
-app.post('/api/signup', function(req, res) {
-  var username = req.body.data.username;
-  var password = req.body.data.password;
-
-  User.findOne({username: username})
-    .exec(function(err, user) {
-      if (!user) {
-        var newUser = new User({
-          username: username,
-          password: password
-        });
-        newUser.save(function(err, newUser) {
-          if (err) { res.status(500).send('this is an error'); }
-          // TODO: create session
-          console.log(`${username} created`);
-          res.send(`${username} created`);
-        });
-      } else {
-        console.log(`username exists`);
-        //res.redirect('/dashboard');
-        res.status(401).send(`username exists`);
-        // TODO: redirect back to the signup page
-      }
-    });
-});
+app.post('/api/signup', handler.signup);
 
 //login route
-app.post('/api/login', function(req, res) {
-  var username = req.body.data.username;
-  var password = req.body.data.password;
-
-  console.log('line 49', username);
-
-  User.findOne({username: username})
-    .exec(function(err, user) {
-      if (!user) {
-        console.log('user does not exist, signup please');
-        // res.redirect('/signup');
-        res.status(403).send('user does not exist, signup please');
-      } else {
-        user.comparePassword(password, user.password, function(err, match) {
-          if (match) {
-            // TODO: take them to dashboard?
-            console.log('line 62: success', username);
-            res.end();
-            // res.redirect('/dashboard');
-          } else {
-            console.log('line 66: incorrect password');
-            //res.redirect('/login');
-            res.status(401).send('incorrect password');
-          }
-        });
-      }
-    });
-});
+app.post('/api/login', handler.login);
 
 //ingredient search api route
-app.post('/api/ingredients', function(req, res) {
-  var ingredient = req.body.data.ingredient;
-
-  Ingredient.findOne({name: ingredient})
-    .exec(function(err, ingredientName) {
-      //if there is an ingredient, return the document JSON, on the front end, we can extrapolate the name and link!
-      if (!ingredientName) {
-        res.status(401).send(`${ingredient} not in database`);
-      } else {
-        res.json(ingredientName);
-      }
-    });
-
-});
+app.post('/api/ingredients', handler.ingredients);
 
 module.exports = app;
