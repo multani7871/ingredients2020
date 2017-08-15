7ef246ea191c3578f8dda9c3823d23ed879469cf
Harvey Multani <harvey.multani@gmail.com>
2017-08-15 03:24:16 -0700

    Comment out tests until ready to work on them

--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
-var request = require('supertest');
-var express = require('express');
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var should = chai.should();
-var expect = require('chai').expect;
-var app = require('../server.js');
-
-chai.use(chaiHttp);
-
-
-it('should not serve the login page since we it doesn\'t exist', (done) => {
-  chai.request(app)
-  .get('/login')
-  .end((err, res) => {
-    res.should.have.status(404);
-    done();
-  });
-});
+// var request = require('supertest');
+// var express = require('express');
+// var chai = require('chai');
+// var chaiHttp = require('chai-http');
+// var should = chai.should();
+// var expect = require('chai').expect;
+// var app = require('../server.js');
+//
+// chai.use(chaiHttp);
+//
+//
+// it('should not serve the login page since we it doesn\'t exist', (done) => {
+//   chai.request(app)
+//   .get('/login')
+//   .end((err, res) => {
+//     res.should.have.status(404);
+//     done();
+//   });
+// });
