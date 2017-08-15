c6b42ae46e0bc96588507f9eadc263092ce784d7
Harvey Multani <harvey.multani@gmail.com>
2017-08-15 03:30:11 -0700

    Clean up comments

--- server-config.js
+++ server-config.js
@@ -1,22 +1,18 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
 var handler = require('./route-handler');
 
 var app = express();
 
+//middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'build')));
 
-//user creation api route
+//routes
 app.post('/api/signup', handler.signup);
-
-//login route
 app.post('/api/login', handler.login);
-
-//ingredient search api route
 app.post('/api/ingredients', handler.ingredients);
 
 module.exports = app;
