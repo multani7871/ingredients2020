4abac9e0f7ede4a1452dfb92dfcb8448a8d57787
Harvey Multani <harvey.multani@gmail.com>
2017-08-16 16:22:59 -0700

    Remove password field, hashing and compare function

--- db/models/users.js
+++ db/models/users.js
@@ -4,29 +4,8 @@ var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
   username: {type: String, required: true, index: {unique: true}},
-  password: {type: String},
-  pastSearches: Array 
+  pastSearches: Array
 }, {collection: 'Users'});
-
-//comparePassword function to come when routing is done
-
-// TODO: upgrade to auth0 or add salts
-
-userSchema.methods.comparePassword = function(candidatePassword, savedPassword, cb) {
-  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
-    if (err) { return cb(err); }
-    cb(null, isMatch);
-  });
-};
-
-userSchema.pre('save', function(next) {
-  var cipher = Promise.promisify(bcrypt.hash);
-  return cipher(this.password, null, null).bind(this)
-    .then(function(hash) {
-      this.password = hash;
-      next();
-    });
-});
 
 var User = mongoose.model('User', userSchema);
 
