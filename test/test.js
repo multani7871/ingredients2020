298cdd21821d613524b84d3fb5ce79f711b5266c
Harvey Multani <harvey.multani@gmail.com>
2017-08-17 11:27:58 -0700

    added example tests

--- test/test.js
+++ test/test.js
@@ -1,19 +1,48 @@
-// var request = require('supertest');
-// var express = require('express');
-// var chai = require('chai');
-// var chaiHttp = require('chai-http');
-// var should = chai.should();
-// var expect = require('chai').expect;
-// var app = require('../server.js');
-//
-// chai.use(chaiHttp);
-//
-//
-// it('should not serve the login page since we it doesn\'t exist', (done) => {
-//   chai.request(app)
-//   .get('/login')
-//   .end((err, res) => {
-//     res.should.have.status(404);
-//     done();
-//   });
-// });
+var request = require('supertest');
+var express = require('express');
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+var expect = require('chai').expect;
+var app = require('../server');
+
+chai.use(chaiHttp);
+
+describe('Authentication', function() {
+  describe('naive', function() {
+
+    it('should not serve the ingredients page w/out authentication', (done) => {
+      chai.request('http://localhost:8000')
+      .get('/api/ingredients')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+    });
+
+    it('should not serve the pastSearches page w/out authentication', (done) => {
+      chai.request('http://localhost:8000')
+      .get('/api/ingredients')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+    });
+
+    it('should not serve the images page w/out authentication', (done) => {
+      chai.request('http://localhost:8000')
+      .get('/api/ingredients')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+
+    });
+
+    describe('todo: database tests in this block', function() {
+
+    });
+
+  });
+
+});
