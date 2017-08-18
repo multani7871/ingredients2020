2aee5f1591e5833f9bdb90909ad2d343790c4c15
Harvey Multani <harvey.multani@gmail.com>
2017-08-18 16:21:46 -0700

    revert to sendFile

--- route-handler.js
+++ route-handler.js
@@ -112,5 +112,5 @@ exports.googleCloudSearch = function(req, res) {
 }
 
 exports.callback = function(req, res) {
-  res.redirect(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 }
