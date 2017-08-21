17ba28ab04b78cf84c795bb7e8b2af44cbf6e96a
Harvey Multani <harvey.multani@gmail.com>
2017-08-21 15:30:57 -0700

    Remove console logs on server/db startup

--- db/config.js
+++ db/config.js
@@ -14,8 +14,5 @@ mongoose.connect(mongoURI,  {
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Mongo connection open');
-});
 
 module.exports = db;
--- server.js
+++ server.js
@@ -2,6 +2,4 @@ var app = require('./server-config');
 
 var port = process.env.PORT || 8000;
 
-app.listen(port, function() {
-  console.log(`listening on ${port}`);
-});
+app.listen(port);
